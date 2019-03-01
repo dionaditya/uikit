a1470e732b2a84f00fb000d21fc4fd8ef5b96b51
Muhamad Firhat <muhamad.firhat@gmail.com>
2019-03-01 17:57:34 +0700

    register new components.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import RfButton from './RfButton'
 import RfTextInput from './RfTextInput'
+import RfContainer from './RfContainer'
+import RfRow from './RfRow'
+import RfCol from './RfCol'
+import RfNav from './RfNav'
+import RfNavBar from './RfNavBar'
 
 import './styles/custom.scss'
 
 const components = [
   RfButton,
   RfTextInput,
+  RfContainer,
+  RfRow,
+  RfCol,
+  RfNav,
+  RfNavBar,
 ]
 
 export {
   RfButton,
-  RfTextInput
+  RfTextInput,
+  RfContainer,
+  RfRow,
+  RfCol,
+  RfNav,
+  RfNavBar,
 }
 
 export default {
