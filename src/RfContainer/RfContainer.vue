7aed1db970fea5bbd3071b811db452cd9a67c30f
Muhamad Firhat <muhamad.firhat@gmail.com>
2019-03-01 17:56:36 +0700

    Create container component.

---
+++ src/RfContainer/RfContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :class="classes"
+    v-on="$listeners"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'RfContainer',
+
+    props: {
+      fluid: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    computed: {
+      classes() {
+        return [
+          this.fluid ? 'container-fluid' : 'container',
+        ]
+      }
+    }
+  }
+</script>
