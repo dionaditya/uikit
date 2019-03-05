df729dfd7209927a73bd27ac60b6f86fffb0b8ab
Muhamad Firhat <muhamad.firhat@gmail.com>
2019-03-05 16:52:17 +0700

    change base on vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/uikit/',
+  base: '/',
   title: "Refactory UI Kit",
   description: "An modular vue component that mainly used on https://refactory.id",
   themeConfig: {
