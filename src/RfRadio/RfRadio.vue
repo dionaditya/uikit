8f15e37f6e2c408a5b3c4c4b79d851b089fba1f2
Muhamad Firhat <muhamad.firhat@gmail.com>
2019-03-15 11:00:53 +0700

    Create RfRadio component.

---
+++ src/RfRadio/RfRadio.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="classes">
+    <input class="form-check-input" type="radio" :name="name" :id="id" :value="value" :disabled="disabled" :checked="checked">
+    <label class="form-check-label" :for="id">{{ label }}</label>
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
+
+<script>
+  export default {
+    name: 'RfRadio',
+
+    props: {
+      name: {
+        type: String,
+        default: '',
+      },
+      id: {
+        type: String,
+        default: '',
+      },
+      label: {
+        type: String,
+        default: '',
+      },
+      value: {
+        type: String,
+        default: '',
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      checked: {
+        type: Boolean,
+        default: false,
+      },
+      inline: {
+        type: Boolean,
+        default: false,
+      }
+    },
+
+    computed: {
+      classes() {
+        const classes = ['form-check']
+        if (this.inline) classes.push('form-check-inline')
+        return classes
+      }
+    }
+  }
+</script>
